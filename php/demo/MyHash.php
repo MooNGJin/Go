38f977e5f5a2118314f7380549f76ad63dd73261
miracle <crazy_zjie@hotmail.com>
2019-03-11 11:20:01 +0800

    format code

--- php/demo/MyHash.php
+++ php/demo/MyHash.php
@@ -8,20 +8,20 @@
 
 class ConsistentHash {
     // server列表
-    private $_server_list = array();
+    private $_server_list = [];
     // 延迟排序，因为可能会执行多次addServer
-    private $_layze_sorted = FALSE;
+    private $_layze_sorted = false;
 
-    public function printServiceList(){
+    public function printServiceList() {
         print_r($this->_server_list);
     }
 
     private function myHash($str) {
         // hash(i) = hash(i-1) * 33 + str[i]
         $hash = 0;
-        $s    = md5($str);
+        $s = md5($str);
         $seed = 5;
-        $len  = 32;
+        $len = 32;
         for ($i = 0; $i < $len; $i++) {
             // (hash << 5) + hash 相当于 hash * 33
             //$hash = sprintf("%u", $hash * 33) + ord($s{$i});
@@ -34,8 +34,7 @@ class ConsistentHash {
 
     public function addServer($server) {
         $hash = $this->myHash($server);
-        $this->_layze_sorted = FALSE;
-
+        $this->_layze_sorted = false;
         if (!isset($this->_server_list[$hash])) {
             $this->_server_list[$hash] = $server;
         }
@@ -47,34 +46,29 @@ class ConsistentHash {
         // 排序
         if (!$this->_layze_sorted) {
             asort($this->_server_list);
-            $this->_layze_sorted = TRUE;
+            $this->_layze_sorted = true;
         }
-
         $hash = $this->myHash($key);
-        $len  = sizeof($this->_server_list);
+        $len = sizeof($this->_server_list);
         if ($len == 0) {
-            return FALSE;
+            return false;
         }
 
-        $keys   = array_keys($this->_server_list);
+        $keys = array_keys($this->_server_list);
         $values = array_values($this->_server_list);
-
         // 如果不在区间内，则返回最后一个server
         if ($hash <= $keys[0] || $hash >= $keys[$len - 1]) {
             return $values[$len - 1];
         }
-
-        foreach ($keys as $key=>$pos) {
-            $next_pos = NULL;
-            if (isset($keys[$key + 1]))
-            {
+        
+        foreach ($keys as $key => $pos) {
+            $next_pos = null;
+            if (isset($keys[$key + 1])) {
                 $next_pos = $keys[$key + 1];
             }
-
             if (is_null($next_pos)) {
                 return $values[$key];
             }
-
             // 区间判断
             if ($hash >= $pos && $hash <= $next_pos) {
                 return $values[$key];
